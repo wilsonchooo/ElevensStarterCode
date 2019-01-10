eb761bae2e28c132fb318eaf5a851f4a2f37478e
BT_1N3_25 <wcho1205@bths.edu>
2019-01-10 11:42:32 -0500

    p

--- src/Activity1/Card.java
+++ src/Activity1/Card.java
@@ -7,35 +7,36 @@ package Activity1;
  */
 public class Card {
 
-    /**
-     * String value that holds the suit of the card
-     */
-    private String suit;
+    public String rank;
+    public String suit;
+    public int pointValue;
+
 
     /**
      * String value that holds the rank of the card
      */
-    private String rank;
 
     /**
      * int value that holds the point value.
      */
-    private int pointValue;
 
 
     /**
      * Creates a new <code>Card</code> instance.
      *
-     * @param cardRank  a <code>String</code> value
+     * @param rank  a <code>String</code> value
      *                  containing the rank of the card
-     * @param cardSuit  a <code>String</code> value
+     * @param suit  a <code>String</code> value
      *                  containing the suit of the card
-     * @param cardPointValue an <code>int</code> value
+     * @param pointValue an <code>int</code> value
      *                  containing the point value of the card
      */
-    public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-    }
+    public Card (String rank, String suit, int pointValue)
+        {
+            this.rank = rank;
+            this.suit = suit;
+            this.pointValue = pointValue;
+        }
 
 
     /**
@@ -43,7 +44,7 @@ public class Card {
      * @return this <code>Card's</code> suit.
      */
     public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return this.suit;
 
     }
 
@@ -53,6 +54,7 @@ public class Card {
      */
     public String rank() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        return this.rank;
 
     }
 
@@ -61,7 +63,7 @@ public class Card {
      * @return this <code>Card's</code> point value.
      */
     public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        return this.pointValue;
 
     }
 
--- src/Activity4/Card.java
+++ src/Activity4/Card.java
@@ -5,76 +5,45 @@ package Activity4;
  *
  * <code>Card</code> represents a playing card.
  */
+
 public class Card {
+    public String rank;
+    public String suit;
+    public int pointValue;
 
-    /**
-     * String value that holds the suit of the card
-     */
-    private String suit;
+    public Card (String rank, String suit, int pointValue)
+    {
+        this.rank = rank;
+        this.suit = suit;
+        this.pointValue = pointValue;
+    }
 
-    /**
-     * String value that holds the rank of the card
-     */
-    private String rank;
+    public String getRank()
+    {
+        return this.rank;
+    }
+    public String getSuit()
+    {
+        return this.suit;
+    }
+    public int getPointValue()
+    {
+        return this.pointValue;
+    }
 
-    /**
-     * int value that holds the point value.
-     */
-    private int pointValue;
+    public boolean equals(Card othercard)
+    {
+        if (pointValue == othercard.pointValue )
+            return true;
 
-
-    /**
-     * Creates a new <code>Card</code> instance.
-     *
-     * @param cardRank  a <code>String</code> value
-     *                  containing the rank of the card
-     * @param cardSuit  a <code>String</code> value
-     *                  containing the suit of the card
-     * @param cardPointValue an <code>int</code> value
-     *                  containing the point value of the card
-     */
-    public Card(String cardRank, String cardSuit, int cardPointValue) {
-        //initializes a new Card with the given rank, suit, and point value
-        rank = cardRank;
-        suit = cardSuit;
-        pointValue = cardPointValue;
+        else return false;
     }
 
 
-    /**
-     * Accesses this <code>Card's</code> suit.
-     * @return this <code>Card's</code> suit.
-     */
-    public String suit() {
-        return suit;
-    }
-
-    /**
-     * Accesses this <code>Card's</code> rank.
-     * @return this <code>Card's</code> rank.
-     */
-    public String rank() {
-        return rank;
-    }
-
-    /**
-     * Accesses this <code>Card's</code> point value.
-     * @return this <code>Card's</code> point value.
-     */
-    public int pointValue() {
-        return pointValue;
-    }
-
-    /** Compare this card with the argument.
-     * @param otherCard the other card to compare to this
-     * @return true if the rank, suit, and point value of this card
-     *              are equal to those of the argument;
-     *         false otherwise.
-     */
-    public boolean matches(Card otherCard) {
-        return otherCard.suit().equals(this.suit())
-                && otherCard.rank().equals(this.rank())
-                && otherCard.pointValue() == this.pointValue();
+   public boolean matches(Card otherCard) {
+        return otherCard.suit.equals(this.suit)
+                && otherCard.rank.equals(this.rank)
+                && otherCard.pointValue == this.pointValue;
     }
 
     /**
